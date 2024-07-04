dd1b73c5e05a2f50c9accaaa1f59d46d67f7962b
daridev <darideveloper@gmail.com>
2024-07-04 15:46:21 -0600

    feat (landing/admin): image and video models

--- landing/admin.py
+++ landing/admin.py
@@ -11,3 +11,15 @@ class CategoryAdmin(admin.ModelAdmin):
 class TextAdmin(admin.ModelAdmin):
     list_display = ['key', 'category', 'value', 'link']
     search_fields = ['key', 'category__name', 'value', 'link']
+    
+    
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['key', 'category', 'image']
+    search_fields = ['key', 'category__name', 'image']
+    
+    
+@admin.register(models.Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['key', 'category', 'video']
+    search_fields = ['key', 'category__name', 'video']
--- landing/models.py
+++ landing/models.py
@@ -26,3 +26,31 @@ class Text(models.Model):
 
     def __str__(self):
         return self.key
+    
+    
+class Image(models.Model):
+    id = models.AutoField(primary_key=True)
+    key = models.CharField(max_length=255, unique=True)
+    category = models.ForeignKey(category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+
+    class Meta:
+        verbose_name = 'Imagen'
+        verbose_name_plural = 'Imágenes'
+    
+    def __str__(self):
+        return self.key
+
+
+class Video(models.Model):
+    id = models.AutoField(primary_key=True)
+    key = models.CharField(max_length=255, unique=True)
+    category = models.ForeignKey(category, on_delete=models.CASCADE)
+    video = models.FileField(upload_to='videos/')
+    
+    class Meta:
+        verbose_name = 'Video'
+        verbose_name_plural = 'Videos'
+    
+    def __str__(self):
+        return self.key
