c9f52b6f24311f9dfb8b5164d7e4238d74846c85
daridev <darideveloper@gmail.com>
2024-07-10 15:50:02 -0600

    feat (middlewere): save cookie with first_name when load admin

--- nyx_dashboard/settings.py
+++ nyx_dashboard/settings.py
@@ -43,6 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'user.middlewere.AdminCookieMiddleware',
 ]
 
 ROOT_URLCONF = 'nyx_dashboard.urls'
---
+++ user/middlewere.py
@@ -0,0 +1,22 @@
+class AdminCookieMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        
+        # Get response
+        
+        response = self.get_response(request)
+        
+        # Validate login view
+        if '/admin' in request.path and request.user.is_authenticated:
+            
+            # Get app base domain
+            response.set_cookie('nyx', request.user.first_name, path='/')
+                
+            print(f'Cookie set: {request.user.first_name}')
+            
+        return response
