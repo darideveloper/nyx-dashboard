12af2e3635b3eb2d6154f8dee4cedaf1f58e1bff
daridev <darideveloper@gmail.com>
2024-07-18 15:10:05 -0600

    test (store/FutureStockTestCase): return future stock date

--- store/tests.py
+++ store/tests.py
@@ -8,10 +8,9 @@ class FutureStockTestCase(TestCase):
     def setUp(self):
         """ Create initial data """
         self.today = timezone.now()
+        self.tomorrow = self.today + timezone.timedelta(days=1)
         self.future_stock = models.FutureStock.objects.create(
-            datetime=self.today + timezone.timedelta(
-                days=3, hours=2, minutes=30, seconds=15
-            ),
+            datetime=self.tomorrow,
             added=False,
             amount=100,
         )
@@ -23,11 +22,11 @@ class FutureStockTestCase(TestCase):
         response = self.client.get(self.endpoint)
         self.assertEqual(response.status_code, 200)
         json_data = response.json()
-        self.assertEqual(json_data["days"], 3)
-        self.assertEqual(json_data["hours"], 2)
-        self.assertEqual(json_data["minutes"], 30)
-        self.assertTrue(json_data["seconds"] <= 15)
-        
+        self.assertEqual(
+            json_data["next_future_stock"],
+            self.tomorrow.strftime('%Y-%m-%d %H:%M:%S')
+        )
+
     def test_get_next_future_stock_with_added(self):
         """ Test when future stock is already added and
         no more future stock to add """
@@ -37,9 +36,8 @@ class FutureStockTestCase(TestCase):
         
         response = self.client.get(self.endpoint)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {
-            "days": 0,
-            "hours": 0,
-            "minutes": 0,
-            "seconds": 0,
-        })
+        json_data = response.json()
+        self.assertEqual(
+            json_data["next_future_stock"],
+            self.today.strftime('%Y-%m-%d %H:%M:%S')
+        )
