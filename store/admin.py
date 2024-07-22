60c78218a1eee029573e3ab5e7ba506b38d09d4e
daridev <darideveloper@gmail.com>
2024-07-22 14:28:14 -0600

    feat (store/admin): add FutureStockSubcription to admin

--- store/admin.py
+++ store/admin.py
@@ -14,3 +14,11 @@ class FutureStockAdmin(admin.ModelAdmin):
     list_display = ('id', 'amount', 'datetime', 'added', 'created_at', 'updated_at')
     search_fields = ('amount', 'datetime', 'added')
     list_filter = ('datetime', 'added', 'created_at', 'updated_at')
+    
+
+@admin.register(models.FutureStockSubcription)
+class FutureStockSubcriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'future_stock', 'active',
+                    'notified', 'created_at', 'updated_at')
+    search_fields = ('user__email', 'future_stock', 'active', 'notified')
+    list_filter = ('active', 'notified', 'created_at', 'updated_at')
