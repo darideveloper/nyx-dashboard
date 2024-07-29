660f4b45e1d729aeb6948482034e258f6e62c64a
daridev <darideveloper@gmail.com>
2024-07-29 17:07:16 -0600

    feat (user/middlewere): save username and email in cookies

--- user/middlewere.py
+++ user/middlewere.py
@@ -19,15 +19,20 @@ class AdminCookieMiddleware:
         # Add or remove cookie
         if '/admin' in request.path and request.user.is_authenticated:
             name = f"{request.user.first_name} {request.user.last_name}"
+            email = request.user.email
             if is_local:
-                response.set_cookie('nyx', name, path='/')
+                response.set_cookie('nyx_username', name, path='/')
+                response.set_cookie('nyx_email', email, path='/')
             else:
-                response.set_cookie('nyx', name, path='/', domain=base_domain)
+                response.set_cookie('nyx_username', name, path='/', domain=base_domain)
+                response.set_cookie('nyx_email', email, path='/', domain=base_domain)
             
         elif "/logout" in request.path:
             if is_local:
-                response.delete_cookie('nyx', path='/')
+                response.delete_cookie('nyx_username', path='/')
+                response.delete_cookie('nyx_email', path='/')
             else:
-                response.delete_cookie('nyx', path='/', domain=base_domain)
+                response.delete_cookie('nyx_username', path='/', domain=base_domain)
+                response.delete_cookie('nyx_email', path='/', domain=base_domain)
             
         return response
