42800f1551b9d6cbed146a866ec1087b258e5d47
daridev <darideveloper@gmail.com>
2024-08-02 09:07:03 -0600

    feat (settings): setup postgresql database

--- nyx_dashboard/settings.py
+++ nyx_dashboard/settings.py
@@ -90,6 +90,14 @@ if IS_TESTING:
         }
     }
 else:
+    
+    options = {}
+    if os.environ.get("DB_ENGINE") == "django.db.backends.mysql":
+        options = {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
+    
     DATABASES = {
         'default': {
             'ENGINE': os.environ.get("DB_ENGINE"),
@@ -98,10 +106,7 @@ else:
             'PASSWORD': os.environ.get("DB_PASSWORD"),
             'HOST': os.environ.get("DB_HOST"),
             'PORT': os.environ.get("DB_PORT"),
-            'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-                'charset': 'utf8mb4',
-            },
+            'OPTIONS': options,
         }
     }
 
