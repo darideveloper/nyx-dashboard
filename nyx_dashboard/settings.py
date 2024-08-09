4a899be5876dd10120437d3d91b7028cd790a8ff
daridev <darideveloper@gmail.com>
2024-08-09 08:34:31 -0600

    fix (settings): update password validators

--- nyx_dashboard/settings.py
+++ nyx_dashboard/settings.py
@@ -104,8 +104,8 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation" \
-                ".UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
