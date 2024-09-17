934279e19c5c438d69ac27ace04a76c283223cee
daridev <darideveloper@gmail.com>
2024-09-16 18:24:43 -0600

    fix (runserver.sh): update project name

--- runserver.sh
+++ runserver.sh
@@ -2,4 +2,4 @@
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py apps_loaddata
-gunicorn djcrm.wsgi --bind=0.0.0.0:80
+gunicorn nyx_dashboard.wsgi --bind=0.0.0.0:80
