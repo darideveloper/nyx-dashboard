cc00a40e219675c723036917f70f099f07ed83e0
daridev <darideveloper@gmail.com>
2024-12-18 10:32:57 -0600

    fix: delete caprover files

--- caprover_scripts/bash.sh
+++
@@ -1,2 +0,0 @@
-# Run it inside vps to get bash access to the container
-docker exec -it $(docker ps -q --filter "name=srv-captain--nyx-dashboard") bash
--- caprover_scripts/logs.bat
+++
@@ -1,2 +0,0 @@
-@REM Show logs of the app
-ssh -i "D:/aws/ec2/daridev-login.pem" ubuntu@ec2-3-12-224-160.us-east-2.compute.amazonaws.com "sudo docker service logs srv-captain--nyx-dashboard --since 10m --follow"
